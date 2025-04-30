9fa50056b03219ac26628ed4151223cdbbade94c
Chidori42 <elfagrouch9@gmail.com>
2025-04-30 23:55:12 +0100

    Cpp08

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: ael-fagr <ael-fagr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 11:04:39 by ael-fagr          #+#    #+#             */
-/*   Updated: 2025/04/30 12:05:13 by ael-fagr         ###   ########.fr       */
+/*   Updated: 2025/04/30 23:54:36 by ael-fagr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,61 +14,15 @@
 #define MUTANTSTACK_HPP
 #include <iostream>
 #include <stack>
+#include <list>
 #include <cstddef>
 
 template<typename T>
-class MutantStack{
-    private:
-        T Data;
-        size_t size;
+class MutantStack : public std::stack<T>{
     public:
-        class iterator {
-            private:
-                T *current;
-            public:
-                iterator& operator++() { ++current; return *this; }
-                iterator operator++(int) { iterator temp(*this); ++(*this); return temp; }
-                iterator& operator--() { --current; return *this; }
-                iterator operator--(int) { iterator temp(*this); --(*this); return temp;}
-                bool operator==(const iterator& other) const { return current == other.current; }
-                bool operator!=(const iterator& other) const { return !(*this == other); }
-                // T operator*(const iterator& other){return ();}
-
-        };
-
-        MutantStack(){
-
-        }
-        ~MutantStack(){
-
-        }
-        MutantStack(const MutantStack &other){
-
-        }
-
-        MutantStack &operator=(const MutantStack &other){
-
-        }
-
-        void push(const T &value){
-
-        }
-        void pop(){
-
-        }
-        T top() const{
-
-        }
-
-        iterator begin() const{
-            
-        }
-        iterator end() const{
-            
-        }
-        std::size_t size() const{
-            
-        }
+        using iterator = typename std::deque<T>::iterator;
+        iterator begin() { return this->c.begin(); }
+        iterator end() { return this->c.end(); }
 };
 
 #endif
